ad364ecbc613d99e3a1e2b6b7e1d61ce41ac6d49
sangeetabhatia03 <sangeetabhatia03@gmail.com>
2018-08-15 15:44:55 +0100

    Forecasts assessment and visualisation of various metrics.

--- analysis/workflow.R
+++ analysis/workflow.R
@@ -13,10 +13,10 @@ apply(pars, 1, function(row) {
 
   tproj <- row[1]
   twindow <- row[2]
-  rmarkdown::render("analysis/extract_mcmc_draws.Rmd",
-  		     params = list(tproj = tproj,
-        	     	           twindow = twindow,
-                                   incid = "healthmap_wide_from_july.csv"))
+  ## rmarkdown::render("analysis/extract_mcmc_draws.Rmd",
+  ## 		     params = list(tproj = tproj,
+  ##       	     	           twindow = twindow,
+  ##                                  incid = "healthmap_wide_from_july.csv"))
 
   rmarkdown::render("analysis/projection_using_fitted.Rmd",
                     params = list(tproj = tproj,
@@ -25,25 +25,45 @@ apply(pars, 1, function(row) {
 
 })
 
-## For a given time window, at every pair of points at which
-## we have done forecasts
+## Now get the assessment metrics
+places <- c("Liberia", "Guinea", "Sierra Leone")
 twindow = seq(from = 7, to = 49, by = 7)
-countries <- c("Guinea", "Liberia", "Sierra Leone")
+pars <- data.frame(t_proj = c(), time_window = c(), place = c())
 for (tw in twindow) {
     t_proj = seq(from = 2 * tw,
                  to = 567,
                  by = tw)
-    pairs <- combn(t_proj, 2)
-    for (c in countries) {
-        apply(pairs, 2, function(col) {
-            rmarkdown::render("analysis/forecasts_visual_check.Rmd",
-                              params = list(tproj = col,
-                                            twindow = tw,
-                                            place = c))
-        })
- }
+    pars <- rbind(pars, expand.grid(t_proj = t_proj,
+                                    time_window = tw,
+                                    place = places))
+}
 
+for (i in 1:nrow(pars)) {
+  row <- pars[i, ]
+  message("Working on row ", row)
+
+  tproj <- row$t_proj
+  twindow <- row$time_window
+  place <- as.character(row$place)
+  rmarkdown::render("analysis/forecasts_assess.Rmd",
+  		     params = list(tproj = tproj,
+        	     	           twindow = twindow,
+                                   n.dates.sim = 28,
+                                   incid = "healthmap_wide_from_july.csv",
+                                   place = place))
 }
 
 
+twindow = seq(from = 7, to = 49, by = 7)
+for (tw in twindow) {
+    tproj <- seq(from = 2 * tw,
+                 to = 560,
+                 by = tw)
 
+    rmarkdown::render("analysis/forecasts_assess_viz.Rmd",
+                      params = list(tproj = tproj,
+                                    twindow = tw,
+                                    n.dates.sim = 28,
+                                    incid = "healthmap_wide_from_july.csv",
+                                    place = "Sierra Leone"))
+}
